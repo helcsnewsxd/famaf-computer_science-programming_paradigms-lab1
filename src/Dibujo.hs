61c488cabc491674d3a610af4f8047ffef867b72
helcsnewsxd <cphelcsnewsxd@gmail.com>
2023-03-28 08:44:52 -0300

    Corregidos algunos comentarios de git merge

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -25,11 +25,7 @@ where
 
 {-
 Gramática de las figuras:
-<<<<<<< HEAD
-<Fig> ::= Figura <Bas> | Rotar <Fig> | Espejar <Fig> | Rot45 <Fig>
-=======
 <Dibujo> ::= Figura <Bas> | Rotar <Fig> | Espejar <Fig> | Rot45 <Fig>
->>>>>>> 5cb508f (Completar tipos en Dibujo y otras cosas)
     | Apilar <Float> <Float> <Fig> <Fig>
     | Juntar <Float> <Float> <Fig> <Fig>
     | Encimar <Fig> <Fig>
@@ -45,55 +41,67 @@ data Dibujo a
   | Encimar (Dibujo a) (Dibujo a)
   deriving (Eq, Show)
 
--- Agreguen los tipos y definan estas funciones
-
 -- Construcción de dibujo. Abstraen los constructores.
 
+figura :: a -> Dibujo a
 figura = Figura
 
+rotar :: Dibujo a -> Dibujo a
 rotar = Rotar
 
+espejar :: Dibujo a -> Dibujo a
 espejar = Espejar
 
+rot45 :: Dibujo a -> Dibujo a
 rot45 = Rot45
 
+apilar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
 apilar = Apilar
 
+juntar :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
 juntar = Juntar
 
+encimar :: Dibujo a -> Dibujo a -> Dibujo a
 encimar = Encimar
 
--- Rotaciones de múltiplos de 90.
-r180 = rotar . rotar
-
 comp :: (a -> a) -> Int -> a -> a
-comp f 1 = f
-comp f i = f . comp f (i-1)
+comp f 1 x = f x
+comp f n x = comp f (n - 1) (f x)
+
+-- Rotaciones de múltiplos de 90.
+r180 :: Dibujo a -> Dibujo a
+r180 = comp rotar 2
 
 r270 :: Dibujo a -> Dibujo a
 r270 = comp rotar 3
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
+(.-.) :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
 (.-.) = apilar
 
 -- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
-(///) = undefined
+(///) :: Float -> Float -> Dibujo a -> Dibujo a -> Dibujo a
+(///) = juntar
 
 -- Superpone una figura con otra.
-(^^^) = undefined
+(^^^) :: Dibujo a -> Dibujo a -> Dibujo a
+(^^^) = encimar
 
 -- Dadas cuatro figuras las ubica en los cuatro cuadrantes.
-cuarteto = undefined
+cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
+cuarteto p q r s = (.-.) 1 1 ((///) 1 1 p q) ((///) 1 1 r s)
 
 -- Una figura repetida con las cuatro rotaciones, superpuestas.
-encimar4 = undefined
+encimar4 :: Dibujo a -> Dibujo a
+encimar4 p = (^^^) (r270 p) ((^^^) (r180 p) ((^^^) (comp rotar 1 p) p))
 
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
-ciclar = undefined
+ciclar :: Dibujo a -> Dibujo a
+ciclar p = cuarteto p (comp rotar 1 p) (r180 p) (r270 p)
 
--- Estructura general para la semántica (a no asustarse). Ayuda:
--- pensar en foldr y las definiciones de Floatro a la lógica
+-- Estructura general para la semántica
+-- Aplico las funciones a cada "nodo" de la estructura del dibujo para devolver un valor
 foldDib ::
   (a -> b) ->
   (b -> b) ->
@@ -104,11 +112,36 @@ foldDib ::
   (b -> b -> b) ->
   Dibujo a ->
   b
-foldDib = undefined
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Figura a) = f_fig a
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Rotar a) = f_rot (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a)
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Espejar a) = f_esp (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a)
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Rot45 a) = f_rot45 (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a)
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Apilar n1 n2 a b) = f_apil n1 n2 (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a) (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim b)
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Juntar n1 n2 a b) = f_junt n1 n2 (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a) (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim b)
+foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim (Encimar a b) = f_encim (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim a) (foldDib f_fig f_rot f_esp f_rot45 f_apil f_junt f_encim b)
 
 -- Demostrar que `mapDib figura = id`
+-- mapDib aplica la función f a todas las figuras (el dato) del Dibujo a para convertirlo en un dibujo b
+-- Por ej.: si tenemos a = Encimar (Figura Rectangulo) (Figura Triangulo), y f una función que cambia Rectángulo a Círculo, entonces
+-- mapDib f a va a ser igual a --> Encimar (Figura Circulo) (Figura Triangulo)
 mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
-mapDib = undefined
+mapDib f (Figura a) = f a
+mapDib f (Rotar a) = Rotar (mapDib f a)
+mapDib f (Espejar a) = Espejar (mapDib f a)
+mapDib f (Rot45 a) = Rot45 (mapDib f a)
+mapDib f (Apilar p1 p2 a b) = Apilar p1 p2 (mapDib f a) (mapDib f b)
+mapDib f (Juntar p1 p2 a b) = Juntar p1 p2 (mapDib f a) (mapDib f b)
+mapDib f (Encimar a b) = Encimar (mapDib f a) (mapDib f b)
 
 -- Junta todas las figuras básicas de un dibujo.
-figuras = undefined
+figurArray :: a -> [a]
+figurArray a = [a]
+
+figurArrayJuntar :: [a] -> [a] -> [a]
+figurArrayJuntar a b = a ++ b
+
+figurArrayJuntar2 :: Float -> Float -> [a] -> [a] -> [a]
+figurArrayJuntar2 _ _ a b = a ++ b
+
+figuras :: Dibujo a -> [a]
+figuras = foldDib figurArray id id id figurArrayJuntar2 figurArrayJuntar2 figurArrayJuntar
