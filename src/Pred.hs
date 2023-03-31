72653a68861b9df511e98f51b4f41334218adb57
EmanuelHerrador <emanuel.nicolas.herrador@unc.edu.ar>
2023-03-31 11:36:02 -0300

    Agregados comentarios en base al docs de la consigna. Implementada allFig

--- src/Pred.hs
+++ src/Pred.hs
@@ -8,21 +8,35 @@ module Pred
   )
 where
 
+-- `Pred a` define un predicado sobre figuras básicas. Por ejemplo,
+-- `(== Triangulo)` es un `Pred TriOCuat` que devuelve `True` cuando la
+-- figura es `Triangulo`.
 type Pred a = a -> Bool
 
--- Dado un predicado sobre básicas, cambiar todas las que satisfacen
--- el predicado por la figura básica indicada por el segundo argumento.
+-- Dado un predicado sobre figuras básicas, cambiar todas las que satisfacen
+-- el predicado por el resultado de llamar a la función indicada por el
+-- segundo argumento con dicha figura.
+-- Por ejemplo, `cambiar (== Triangulo) (\x -> Rotar (Figura x))` rota
+-- todos los triángulos.
+cambiar :: Pred a -> (a -> Dibujo a) -> Dibujo a -> Dibujo a
 cambiar = undefined
 
 -- Alguna básica satisface el predicado.
-anyDib = undefined
+anyFig :: Pred a -> Dibujo a -> Bool
+anyFig = undefined
 
 -- Todas las básicas satisfacen el predicado.
-allDib = undefined
+-- Se aplica la función f_pred en cada Figura del Dibujo a
+-- Las demás funciones lambda se encargan de hacer el and correspondiente
+-- para chequear que se cumpla en TODAS las figuras
+allFig :: Pred a -> Dibujo a -> Bool
+allFig f_pred a = foldDib f_pred id id id (\ _ _ a b -> a&&b) (\ _ _ a b -> a&&b) (&&)
 
 -- Los dos predicados se cumplen para el elemento recibido.
+andP :: Pred a -> Pred a -> Pred a
 andP = undefined
 
 -- Algún predicado se cumple para el elemento recibido.
+orP :: Pred a -> Pred a -> Pred a
 orP = undefined
 
