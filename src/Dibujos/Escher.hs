a5354ff929ef6c59cde7244796f5b3e1b726c5c7
wilitp <guilledeipola@gmail.com>
2023-04-10 19:53:36 -0300

    Dibujo Squarelimit WIP

--- src/Dibujos/Escher.hs
+++ src/Dibujos/Escher.hs
@@ -3,9 +3,9 @@ module Dibujos.Escher
   )
 where
 
-import Dibujo (Dibujo, figura, rotar, rot45, r180, r270, ciclar, cuarteto, (.-.), (///), apilar, juntar)
+import Dibujo (Dibujo, figura, rotar, rot45, r180, r270, ciclar, cuarteto, (.-.), (///), apilar, juntar, encimar, espejar)
 import FloatingPic (Output, half, zero)
-import Graphics.Gloss (Picture (Blank), blue, color, line, pictures, red, white)
+import Graphics.Gloss (Picture (Blank), blue, color, line, pictures, red, white, polygon)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
 import Grilla (grilla)
 import Interp (Conf (..), interp)
@@ -18,32 +18,38 @@ p = figura True
 
 -- El dibujoU.
 dibujoU :: Dibujo Escher
-dibujoU = rot45 (r180 (ciclar p))
+dibujoU = ciclar p2
+  where p2 = espejar $ rot45 p
 
 -- El dibujo t.
 dibujoT :: Dibujo Escher
-dibujoT = rot45 ((.-.) p (r270 p))
+-- dibujoT = encimar p $ encimar (r270 (rot45 p)) $ espejar $ rot45 p
+dibujoT = encimar p $ encimar p2 p3
+  where
+    p2 = espejar $ rot45 p
+    p3 = r270 p2
 
 dibujoV = ciclar $ rotar dibujoT
 
 -- Esquina con nivel de detalle en base a la figura p.
 esquina :: Int -> Dibujo Escher
-esquina 1 = cuarteto (figura False) (figura False) (figura False) dibujoU
+esquina 0 = cuarteto (figura False) (figura False) (figura False) dibujoU
 esquina n = cuarteto (esquina (n-1)) (lado (n-1)) (rot45 (lado (n-1))) dibujoU
 
 -- Lado con nivel de detalle.
 lado :: Int -> Dibujo Escher
-lado 1 = cuarteto (figura False) (figura False) (rot45 dibujoT) dibujoT
+lado 0 = cuarteto (figura False) (figura False) (rot45 dibujoT) dibujoT
 lado n = cuarteto (lado (n-1)) (lado (n-1)) (rot45 dibujoT) dibujoT
 
 -- Arreglo de 9 en grilla
-nonet p q r s t u v w x = 
-  apilar 1 2
-    (juntar 1 2 p ((///) q r))
-    ((.-.)
-      (juntar 1 2 s ((///) t u))
-      (juntar 1 2 v ((///) w x))
-    )
+-- nonet p q r s t u v w x = 
+--   apilar 1 2
+--     (juntar 1 2 p ((///) q r))
+--     ((.-.)
+--       (juntar 1 2 s ((///) t u))
+--       (juntar 1 2 v ((///) w x))
+--     )
+nonet p q r s t u v w x = grilla [[p, q, r], [s, t, u], [v, w, x]]
 
 -- El dibujo de Escher:
 squarelimit :: Int -> Dibujo Escher
@@ -56,12 +62,23 @@ squarelimit n =
 
 interpBas :: Output Escher
 interpBas False x y z = Blank
-interpBas True x y z = Blank -- Cambiar esto para que dibujo un triangulo o un pecesito
+-- interpBas true a b c = polygon $ triangulo a b c
+--   where
+--     triangulo a b c = map (a V.+) [zero, c, b, zero]
+interpBas True a b c = pictures [line $ triangulo a b c, cara a b c]
+  where
+    triangulo a b c = map (a V.+) [zero, c, b, zero]
+    cara a b c = polygon $ triangulo (a V.+ half c) (half b) (half c)
+
+dibujito = squarelimit 1
+-- dibujito = encimar p $ r180 p
+-- dibujito = dibujoV
 
 -- Configuración del dibujo de Escher
 escherConf :: Conf
 escherConf =
   Conf
     { name = "Escher",
-      pic = interp interpBas (squarelimit 2)
+      pic = interp interpBas dibujito
+      -- pic = interp interpBas (figura True)
     }
