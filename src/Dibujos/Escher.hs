f2bad18384b69dcdf69cf1da8edb0f8f80828a3e
wilitp <guilledeipola@gmail.com>
2023-04-10 14:03:52 -0300

    Terminar de definir squarelimit como en el paper

--- src/Dibujos/Escher.hs
+++ src/Dibujos/Escher.hs
@@ -5,7 +5,7 @@ where
 
 import Dibujo (Dibujo, figura, rotar, rot45, r180, r270, ciclar, cuarteto, (.-.), (///), apilar, juntar)
 import FloatingPic (Output, half, zero)
-import Graphics.Gloss (Picture, blue, color, line, pictures, red, white)
+import Graphics.Gloss (Picture (Blank), blue, color, line, pictures, red, white)
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
 import Grilla (grilla)
 import Interp (Conf (..), interp)
@@ -13,35 +13,30 @@ import Interp (Conf (..), interp)
 -- Tipo de dato de Escher (vacio o no)
 type Escher = Bool
 
+p :: Dibujo Escher
+p = figura True
+
 -- El dibujoU.
-dibujoU :: Dibujo Escher -> Dibujo Escher
-dibujoU p = rot45 (r180 (ciclar p))
+dibujoU :: Dibujo Escher
+dibujoU = rot45 (r180 (ciclar p))
 
 -- El dibujo t.
-dibujoT :: Dibujo Escher -> Dibujo Escher
-dibujoT p = rot45 ((.-.) p (r270 p))
+dibujoT :: Dibujo Escher
+dibujoT = rot45 ((.-.) p (r270 p))
+
+dibujoV = ciclar $ rotar dibujoT
 
 -- Esquina con nivel de detalle en base a la figura p.
-esquina :: Int -> Dibujo Escher -> Dibujo Escher
-esquina n f | n == 1 = cuarteto (figura False) (figura False) (figura False) (dibujoU f)
-            | otherwise = cuarteto (esquina (n-1) f) (lado (n-1) f) (rot45 (lado (n-1) f)) (dibujoU f)
+esquina :: Int -> Dibujo Escher
+esquina 1 = cuarteto (figura False) (figura False) (figura False) dibujoU
+esquina n = cuarteto (esquina (n-1)) (lado (n-1)) (rot45 (lado (n-1))) dibujoU
 
 -- Lado con nivel de detalle.
-lado :: Int -> Dibujo Escher -> Dibujo Escher
-lado n f | n == 1 = cuarteto (figura False) (figura False) (rot45 f) f
-         | otherwise = cuarteto (lado (n-1) f) (lado (n-1) f) (rot45 f) f
+lado :: Int -> Dibujo Escher
+lado 1 = cuarteto (figura False) (figura False) (rot45 dibujoT) dibujoT
+lado n = cuarteto (lado (n-1)) (lado (n-1)) (rot45 dibujoT) dibujoT
 
--- Por suerte no tenemos que poner el tipo!
--- nonet(p, q, r,
---       s, t, u,
---       v, w, x) =
---          above(1, 2,
---            beside(1, 2, p, beside(1, 1, q, r)),
---            above(1, 1,
---              beside(1, 2, s, beside(1, 1, t, u)),
---              beside(1, 2, v, beside(1, 1, w, x))
---            )
---          )
+-- Arreglo de 9 en grilla
 nonet p q r s t u v w x = 
   apilar 1 2
     (juntar 1 2 p ((///) q r))
@@ -51,25 +46,22 @@ nonet p q r s t u v w x =
     )
 
 -- El dibujo de Escher:
--- squarelimit =
---    nonet(corner, side, rot(rot(rot(corner)))
---            rot(side), u, rot(rot(rot(side))),
---            rot(corner), rot(rot(side)), rot(rot(corner))
---    )
--- ES LO PUESTO EN EL PDF DEL TRABAJO, ADAPTAR A ESCHER
-escher :: Int -> Escher -> Dibujo Escher
-escher n p = 
+squarelimit :: Int -> Dibujo Escher
+squarelimit n = 
   nonet
-    (esquina n dp) (lado n dp) (r270 (esquina n dp))
-    (rotar (lado n dp)) (dibujoU dp) (r270 (lado n dp))
-    (rotar (esquina n dp)) (r180 (lado n dp)) (r180 (esquina n dp))
-  where
-    dp = figura p
+    (esquina n) (lado n) (r270 (esquina n))
+    (rotar (lado n )) dibujoU  (r270 (lado n ))
+    (rotar (esquina n )) (r180 (lado n )) (r180 (esquina n ))
+
+
+interpBas :: Output Escher
+interpBas False x y z = Blank
+interpBas True x y z = Blank -- Cambiar esto para que dibujo un triangulo o un pecesito
 
 -- Configuración del dibujo de Escher
 escherConf :: Conf
 escherConf =
   Conf
-    { name = "Escher"
-      -- FALTA COMPLETAR
+    { name = "Escher",
+      pic = interp interpBas (squarelimit 2)
     }
